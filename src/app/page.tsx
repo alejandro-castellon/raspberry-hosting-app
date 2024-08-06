0f81f5a032ef9fa0b0010d85ae3749a5bbb6561f
Alejandro C <alejandrocastellonfer@gmail.com>
2024-08-06 06:22:02 +0100

    implement camera streaming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import Link
+ from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Raspberry pi</h1>
+      <div className="flex mt-10"><Link href={"/stats"}><Button className="mx-2">Stats</Button> </Link>
+      <Link href={"/camera"}><Button>Camera</Button></Link></div>
+      
     </main>
   );
 }
